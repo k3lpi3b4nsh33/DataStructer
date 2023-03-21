fbee6585e82dcbb177955dc7ac2dfb674c219400
StarGrandp4 <1017579727@qq.com>
2023-03-21 16:35:07 +0800

    Double List Define

---
+++ 2. LinkList/Double List/DList.h
@@ -0,0 +1,28 @@
+/**
+ * @file DList.h
+ * @author kelpie (kelpie@skiff.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+
+#define Elemtype int
+
+// The list node
+typedef struct DlistNode{
+    struct DlistNode *prev;
+    struct DlistNode *next;
+    Elemtype data;
+} stDlistNode;
+
+// The whole list
+typedef struct Dlisthead{ 
+    int size;
+    stDlistNode *head;
+    stDlistNode *tail;
+} stDlistHead;
